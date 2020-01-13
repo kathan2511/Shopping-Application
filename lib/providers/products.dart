3a2fb5d8b77ad825ceef80726ffa9cfa4093b59e
Kathan Shah <kathan2511@gmail.com>
2020-01-13 09:01:30 +0530

    Third Commit

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -65,6 +67,14 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
+    const url ='https://shopping-application-481ec.firebaseio.com/products.json';
+    http.post(url,body: json.encode({
+      'title': product.title,
+      'description': product.description,
+      'price': product.price,
+      'imageUrl': product.imageUrl,  
+      'isFavorite' : product.isFavorite,
+    }),);
     final newProduct = Product(
       title: product.title,
       imageUrl: product.imageUrl,
